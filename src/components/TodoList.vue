6c5782215ab23b538c2c394ee15bdd2d6f55b0a2
Angus Wu <s0937196597@gmail.com>
2023-02-03 17:40:10 +0800

    add function create/del/edit/finish/redo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,33 @@
 <script setup>
 import { reactive, ref } from 'vue';
-const data = reactive([
-  {
-    title: 'first',
+const data = reactive([]);
+const inputTask = ref('');
+const inputEdit = ref('');
+const createTask = () => {
+  let tempData = {
+    title: inputTask.value,
     checked: false,
-  },
-  {
-    title: 'second',
-    checked: true,
-  },
-]);
+  };
+  data.push(tempData);
+  inputTask.value = '';
+};
+const finishTask = (index) => {
+  data[index].checked = !data[index].checked;
+};
+const deleteTask = (index) => {
+  data.splice(index, 1);
+};
+const btnEditTask = (index) => {
+  inputEdit.value = data[index].title;
+  data[index].isEditing = true;
+};
+const editTask = (index) => {
+  data[index].title = inputEdit.value;
+  data[index].isEditing = false;
+};
+const clearAllTask = () => {
+  data.splice(0);
+};
 </script>
 
 <template>
@@ -22,40 +40,70 @@ const data = reactive([
             class="w-80 border-b-2 border-gray-500 text-black p-3"
             type="text"
             placeholder="輸入待辦事項"
+            v-model="inputTask"
           />
           <button
             class="ml-2 border-2 bg-green-500 text-white p-3 font-bold hover:bg-green-600 rounded-lg flex"
+            @click="createTask"
           >
             Add
           </button>
         </div>
       </div>
       <div class="mt-8">
-        <ul>
+        <div
+          class="text-center font-bold text-2xl text-gray-700"
+          v-if="data.length === 0"
+        >
+          趕快新增待辦事項吧 !
+        </div>
+        <ul v-else>
           <li class="p-2 rounded-lg" v-for="(item, index) in data" :key="index">
             <div class="flex align-middle flex-row justify-between">
               <div class="p-2">
-                <p class="text-lg font-bold" :class="{'line-through':item.checked,'text-gray-400':item.checked}">{{ item.title }}</p>
+                <input
+                  type="text"
+                  class="px-1 border-2 border-blue-500 focus:outline-none focus:border-blue-500"
+                  v-if="item.isEditing"
+                  v-model="inputEdit"
+                  @keyup.enter="editTask(index)"
+                />
+                <p
+                  v-else
+                  class="text-lg font-bold"
+                  :class="{
+                    'line-through': item.checked,
+                    'text-gray-400': item.checked,
+                  }"
+                >
+                  {{ item.title }}
+                </p>
               </div>
               <div class="flex">
                 <button
                   v-if="!item.checked"
                   class="border-2 bg-green-400 text-white p-2 font-bold hover:bg-green-500 rounded-lg flex"
+                  @click="finishTask(index)"
                 >
                   Finish
                 </button>
                 <button
                   v-else
                   class="border-2 bg-yellow-400 text-white p-2 font-bold hover:bg-yellow-500 rounded-lg flex"
+                  @click="finishTask(index)"
                 >
                   Redo
                 </button>
                 <button
                   class="ml-2 border-2 bg-blue-400 text-white p-2 font-bold hover:bg-blue-500 rounded-lg flex"
+                  @click="btnEditTask(index)"
                 >
                   Edit
                 </button>
-                <button class="flex text-red-500 p-2 rounded-lg">
+                <button
+                  class="flex text-red-500 p-2 rounded-lg"
+                  @click="deleteTask(index)"
+                >
                   <svg
                     class="h-6 w-6 text-red-500 hover:text-red-700"
                     viewBox="0 0 24 24"
@@ -79,6 +127,7 @@ const data = reactive([
       <div class="mt-8">
         <button
           class="border-2 font-bold border-red-500 p-2 text-red-500 hover:bg-red-600 hover:text-white rounded-lg"
+          @click="clearAllTask"
         >
           Clear All Task
         </button>
